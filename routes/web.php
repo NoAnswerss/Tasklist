9648df9b7457fcb759a5060f7dba355e80c07c50
noanswerss <ciprianvlad888@gmail.com>
2024-02-12 20:18:03 +0000

    installed tailwind

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 
@@ -14,8 +15,7 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::get('/', function () { return view('welcome'); });
-
-Route::post('/task', function (Request $request){ });
-
-Route::delete('/task/{tast}', function (Task $task){ });
+Route::get('/',[TaskController::class, 'index']) -> name('index');
+Route::post('/', [TaskController::class, 'store']) -> name('store');
+Route::post('/complete{tasklist}', [TaskController::class, 'complete']) -> name('complete');
+Route::delete('{todolist:id}', [TaskController::class, 'delete']) -> name('delete');
